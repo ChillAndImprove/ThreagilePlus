2a42fa0836d647559cb581bd8dc823e66c8998c4
Sebastian Geröly <sebastian.geroely@lgln.niedersachsen.de>
2023-06-09 21:53:01 +0200

    refactor: change some more

--- go-mod-tidy.sh
+++
@@ -1,8 +0,0 @@
-# clean the mod cache
-#go clean -modcache
-# set the minimum go version to use for the module
-go mod edit -go=1.16
-# add missing and remove unused modules
-go mod tidy
-# update all dependencies
-go get -u ./...
