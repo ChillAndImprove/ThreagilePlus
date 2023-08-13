98a79ca140600049a6ff6bf2c893aa4930afb075
vd <wolga777@example.com>
2023-08-13 15:31:55 +0200

    tests rock

--- www/tests/tests.py
+++ www/tests/tests.py
@@ -48,6 +48,31 @@ class MyTestCase(unittest.TestCase):
             cls.driver.switch_to.default_content()
         finally:
             print("failed")
+    '''
+    def test_aselect_confidential(self):
+        try:
+            # Click on the specified element
+            element_to_click = self.driver.find_element(By.CSS_SELECTOR, ".geDiagramContainer > svg:nth-child(2) > g:nth-child(1) > g:nth-child(2) > g:nth-child(18) > g:nth-child(1) > text:nth-child(1)")
+            element_to_click.click()
+
+            time.sleep(2) # Wait to allow the dropdown to load
+
+            # Find the select element
+            select_element = self.driver.find_element(By.CSS_SELECTOR, "div.geSidebarContainer:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > li:nth-child(1) > div:nth-child(2) > select:nth-child(1)")
+
+            # Use the Select class to choose "confidential" from the dropdown
+            from selenium.webdriver.support.ui import Select
+            select = Select(select_element)
+            select.select_by_visible_text('confidential')
+
+            # Check that "confidential" was indeed selected
+            selected_option = select.first_selected_option
+            self.assertEqual(selected_option.text, 'confidential', msg="The 'confidential' option was not selected")
+
+        except Exception as e:
+            print(f"Test failed with error: {e}")
+            raise
+'''
     def test_data_assets(self):
         try:
             time.sleep(2)
@@ -65,6 +90,9 @@ class MyTestCase(unittest.TestCase):
             textarea_content = self.driver.find_element(By.CSS_SELECTOR, ".geDialog > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > textarea:nth-child(1)").text
 
             self.assertNotEqual(textarea_content, "E.g. Element1", msg="Unexpected content in the textarea")
+            close_button = self.driver.find_element(By.CSS_SELECTOR, "button.geBtn:nth-child(1)")
+            close_button.click()
+
         except Exception as e:
             print(f"Test failed with error: {e}")
             raise
